08cd5256be0f70b7d5f1e0e3dc2bb9c250dc4eee
DarCkly666 <darckly6@gmail.com>
2025-03-02 21:27:40 -0400

    Radio crud

--- src/routes/radio.router.js
+++ src/routes/radio.router.js
@@ -1,7 +1,13 @@
 import { Router } from 'express'
-import { getRadios, showSave } from '../controllers/radio.controller.js'
+import { getRadios, showSave, save, showEdit, edit, showRemove, remove, showById } from '../controllers/radio.controller.js'
 
 export const radioRouter = Router()
 
 radioRouter.get('/', getRadios)
 radioRouter.get('/new', showSave)
+radioRouter.post('/new', save)
+radioRouter.get('/edit/:id', showEdit)
+radioRouter.post('/edit/:id', edit)
+radioRouter.get('/delete/:id', showRemove)
+radioRouter.post('/delete/:id', remove)
+radioRouter.get('/:id', showById)
